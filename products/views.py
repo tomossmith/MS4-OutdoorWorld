875790f7c7e5c3d00e445dcb85fc48908e5ba3f1
Tom Smith <tomossmith86@icloud.com>
2022-01-04 11:40:28 +0000

    added sorting and search result information on product pages

--- products/views.py
+++ products/views.py
@@ -22,6 +22,9 @@ def all_products(request):
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
 
+            if sortkey == 'category':
+                sortkey = 'category__name'
+
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':

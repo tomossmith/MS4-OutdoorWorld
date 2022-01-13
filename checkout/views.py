e42de95a46bf9a86b79ada6ec3cedcf1f194b192
Tom Smith <tomossmith86@icloud.com>
2022-01-13 17:35:37 +0000

    added form data to checkout statement

--- checkout/views.py
+++ checkout/views.py
@@ -11,21 +11,40 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    bag = request.session.get('bag', {})
-    if not bag:
-        message.error(request, "There's nothing in your bag at the moment")
-        return redirect (reverse('products'))
+    if request.method == 'POST':
+        bag = request.session.get('bag', {})
 
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_Address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = Orderform(form_data)
+        if order_form_is_valid():
+            order_form.save()
+            for item_id, item_data in bag.items
+    else:
+        bag = request.session.get('bag', {})
+        if not bag:
+            message.error(request, "There's nothing in your bag at the moment")
+            return redirect (reverse('products'))
 
-    order_form = OrderForm()
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        order_form = OrderForm()
     
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing, \

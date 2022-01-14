5667eda81ad1c0e16d51c75dadabda7f3e11fbef
Tom Smith <tomossmith86@icloud.com>
2022-01-14 10:39:15 +0000

    code tidying and repairing typying errors

--- checkout/signals.py
+++ checkout/signals.py
@@ -8,11 +8,11 @@ def update_on_save(sender, instance, created, **kwargs):
     """
     Update order total on lineitem update/create
     """
-    instance.order.update_()
+    instance.order.update_total()
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, instance, **kwargs):
+def update_on_delete(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
     """
-    instance.order.update_()
+    instance.order.update_total()
--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,7 @@ from bag.contexts import bag_contents
 
 import stripe
 
+
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
@@ -23,13 +24,13 @@ def checkout(request):
             'country': request.POST['country'],
             'postcode': request.POST['postcode'],
             'town_or_city': request.POST['town_or_city'],
-            'street_Address1': request.POST['street_address1'],
+            'street_address1': request.POST['street_address1'],
             'street_address2': request.POST['street_address2'],
             'county': request.POST['county'],
         }
-        order_form = Orderform(form_data)
-        if order_form_is_valid():
-            order_form.save()
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
@@ -57,7 +58,7 @@ def checkout(request):
                     order.delete()
                     return redirect(reverse('view_bag'))
 
-            request.session['save_info'] in request.POST
+            request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
             messages.error(request, 'There was an error with your form, \
@@ -65,14 +66,14 @@ def checkout(request):
     else:
         bag = request.session.get('bag', {})
         if not bag:
-            message.error(request, "There's nothing in your bag at the moment")
-            return redirect (reverse('products'))
+            messages.error(request, "There's nothing in your bag at the moment")
+            return redirect(reverse('products'))
 
         current_bag = bag_contents(request)
         total = current_bag['grand_total']
         stripe_total = round(total * 100)
         stripe.api_key = stripe_secret_key
-        stripe.PaymentIntent.create(
+        intent = stripe.PaymentIntent.create(
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
         )
@@ -86,8 +87,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'stripe_public_key',
-        'client_secret': 'intent.client_secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
@@ -99,7 +100,7 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-    messages.success(Request, f'Order successfully processed! \
+    messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}')
 

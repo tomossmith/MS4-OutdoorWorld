a43416d02ba841312f56969265289bb04eae43b8
Tom Smith <tomossmith86@icloud.com>
2022-01-18 17:57:42 +0000

    updated checkout form

--- checkout/forms.py
+++ checkout/forms.py
@@ -16,28 +16,24 @@ class OrderForm(forms.ModelForm):
         labels and set autofocus on first field
         """
         super().__init__(*args, **kwargs)
-        """ Placeholders for form fields """
         placeholders = {
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County',
+            'county': 'County, State or Locality',
         }
 
-        """ Focuses the cursor onto the field on page load """
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            """ sets '*' to all required fields """
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                """ adds placeholder text"""
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False

289c932368a9b34fcf0a2c9fb05fdac4c5f2e2ef
Tom Smith <tomossmith86@icloud.com>
2022-01-26 15:27:29 +0000

    added 'delete product' view and url

--- products/views.py
+++ products/views.py
@@ -78,9 +78,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Product was successfully added to the store!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -116,3 +116,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product was successfully deleted from the store.')
+    return redirect(reverse('products'))

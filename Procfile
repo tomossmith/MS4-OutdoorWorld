8265b15b72ced824970117d0bba5fb3444de5434
Tom Smith <tomossmith86@icloud.com>
2022-01-26 16:42:23 +0000

    continue deploying to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn outdoorworld.wsgi:application
--- ms4/settings.py
+++ ms4/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-yfz-m(i2=2iuw#^gl4k^#dc_c^ug@*didm)@t8p7hr5dwi!n8*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ts-outdoorworld.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,12 +119,17 @@ WSGI_APPLICATION = 'ms4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_Database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation

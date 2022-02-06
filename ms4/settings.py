b76b8c4900311c115dc20f9c36f3e1562bf3b17b
Tom Smith <tomossmith86@icloud.com>
2022-02-06 21:49:43 +0000

    fixed typo in settings.py

--- ms4/settings.py
+++ ms4/settings.py
@@ -181,10 +181,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    # CACHE CONTROL
+    # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu 31 Dec 2099 20:00:00 GMT',
-        'Cachecontrol': 'max-age=94608000',
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
     }
 
     # AWS BUCKET CONFIG

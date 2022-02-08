ee682296cdca4f54cfd0e72ee651e9819646b184
Tom Smith <tomossmith86@icloud.com>
2022-02-08 12:41:08 +0000

    added 'brand' to product templates

--- ms4/settings.py
+++ ms4/settings.py
@@ -25,7 +25,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
-#DEBUG = True
 
 ALLOWED_HOSTS = ['ts-outdoorworld.herokuapp.com', 'localhost']
 

8f30edc1a9d56382ea0f6f2a8112bc6d7649bb88
Tom Smith <tomossmith86@icloud.com>
2022-02-08 12:19:38 +0000

    added brand to products and removed links from navbar

--- ms4/settings.py
+++ ms4/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
+#DEBUG = True
 
 ALLOWED_HOSTS = ['ts-outdoorworld.herokuapp.com', 'localhost']
 
--- products/models.py
+++ products/models.py
@@ -19,6 +19,7 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
+    brand = models.CharField(max_length=254)
     name = models.CharField(max_length=254)
     description = models.TextField()
     has_sizes = models.BooleanField(default=False, null=True, blank=True)

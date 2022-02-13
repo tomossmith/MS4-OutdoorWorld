95384cd3cf3ff055daf3ec5c93dcd860442a8006
Tom Smith <tomossmith86@icloud.com>
2022-02-13 18:56:38 +0000

    added contact page to footer,urls and views

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('contact', views.contact, name='contact')
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,8 @@ def index(request):
     """ A view to return the index page """
     
     return render(request, 'home/index.html')
+
+def contact(request):
+    """ A view to display a contact form """
+
+    return render(request, 'home/contact.html')

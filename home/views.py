04f44018549efc3a1bf48b5b304dde9a581af3ab
Tom Smith <tomossmith86@icloud.com>
2022-02-14 15:00:22 +0000

    updated contact form

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
-from django.contrib import messages
-from django.conf import settings
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -11,11 +12,23 @@ def index(request):
     return render(request, 'home/index.html')
 
 def contact(request):
-    """ A view to return the index page """
+	if request.method == 'POST':
+		form = ContactForm(request.POST)
+		if form.is_valid():
+			subject = "Website Query" 
+			body = {
+			'first_name': form.cleaned_data['first_name'], 
+			'last_name': form.cleaned_data['last_name'], 
+			'email': form.cleaned_data['email_address'], 
+			'message':form.cleaned_data['message'], 
+			}
+			message = "\n".join(body.values())
 
-    if request.method == 'POST':
-        messages.error(request, 'Thank you for contacting us, we aim to respond within \
-            2 working days.')
-        return render(request, 'home/contact.html')         
-    
-    return render(request, 'home/contact.html')
+			try:
+				send_mail(subject, message, 'admin@outdoorworld.com', ['to_email']) 
+			except BadHeaderError:
+				return HttpResponse('Invalid header found.')
+			return redirect ("main:homepage")
+      
+	form = ContactForm()
+	return render(request, "home/contact.html", {'form':form})

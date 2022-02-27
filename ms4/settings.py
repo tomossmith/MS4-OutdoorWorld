b6dc8581e411f5fb9d176124342cfc5fcb583710
Tom Smith <tomossmith86@icloud.com>
2022-02-27 17:34:38 +0000

    updated css for allauth buttons

--- ms4/settings.py
+++ ms4/settings.py
@@ -230,3 +230,4 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    

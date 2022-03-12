bdf394a08fcbba2f8c4a563659065b388728cc3c
Tom Smith <tomossmith86@icloud.com>
2022-03-12 14:39:48 +0000

    added contact email template

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.core.mail import send_mail
+from django.template.loader import get_template
 from django.conf import settings
 from .models import Contact
 from .forms import ContactForm
@@ -9,19 +10,30 @@ from .forms import ContactForm
 def contact(request):
     """ contact form """
     if request.method == "POST":
-        EMAIL_HOST_USER = "outdoorworldms4@gmail.com"
         form = ContactForm(request.POST, request.FILES)
         if form.is_valid():
             contact = form.save(commit=False)
-            # form.save()
             name = request.POST.get('name')
             email = request.POST.get('email')
             subject = request.POST.get('subject')
             message = request.POST.get('message')
 
+            #send_mail(
+            #    subject,
+            #    message,
+            #    settings.EMAIL_HOST_USER, [email], fail_silently=True
+            #)
+
+
             send_mail(
-                subject,
-                message,
+                'Contact Form Submission',
+                get_template('contact/templates/email/contact-form-reply.html')
+                .render(
+                    {
+                        "name": name,
+                        "subject": subject,
+                    }
+                ),
                 settings.EMAIL_HOST_USER, [email], fail_silently=True
             )
 

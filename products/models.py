90491fb26043ce041d297a6adfb064f5f8afb7de
Tom Smith <tomossmith86@icloud.com>
2022-03-16 21:18:20 +0000

    removed rating from product model

--- products/models.py
+++ products/models.py
@@ -29,7 +29,6 @@ class Product(models.Model):
     description = models.TextField()
     has_sizes =  models.CharField(max_length=15, choices=SIZE_CHOICES, default='no-sizing')
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 

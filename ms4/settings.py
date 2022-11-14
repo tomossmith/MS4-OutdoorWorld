36073427e4f612e0ef914b8bac114dbc2152b799
Tom Smith <tomossmith86@icloud.com>
2022-11-14 22:34:49 +0000

    fixed indentation error

--- ms4/settings.py
+++ ms4/settings.py
@@ -27,10 +27,10 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ts-outdoorworld.herokuapp.com', 'localhost']
 
- # Add Render.com URL to allowed hosts
- RENDER_EXTERNAL_HOSTNAME = os.environ.get('https://ms4-outdoorworld.onrender.com/')
- if RENDER_EXTERNAL_HOSTNAME:
-   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('https://ms4-outdoorworld.onrender.com/')
+if RENDER_EXTERNAL_HOSTNAME:
+  ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 

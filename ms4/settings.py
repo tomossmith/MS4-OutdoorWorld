480032448f9d09a4f296992fd26ae061d2952ac3
Tom Smith <tomossmith86@icloud.com>
2024-07-18 13:07:10 +0000

    fixed bug in settings.py

--- ms4/settings.py
+++ ms4/settings.py
@@ -169,6 +169,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

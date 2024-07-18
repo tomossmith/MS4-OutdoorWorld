4d2cd8d91a05b8dac5f9a8817e228b72b0bf252f
Tom Smith <tomossmith86@icloud.com>
2024-07-18 12:26:21 +0000

    ammendments to settings.py to push to render

--- build.sh
+++ build.sh
@@ -1,12 +1,4 @@
-#!/usr/bin/env bash
-# Exit on error
-set -o errexit
-
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-pip3 install -r requirements.txt
-
-# Convert static asset files
-#python manage.py collectstatic --no-input
-
-# Apply any outstanding database migrations
-#python manage.py migrate
+ set -o errexit
+ pip install -r requirements.txt
+ python manage.py collectstatic --noinput
+ python manage.py makemigrations && python manage.py migrate

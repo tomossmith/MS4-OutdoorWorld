4575efa559bb851589c92e4ffc7a98616878b753
Tom Smith <tomossmith86@icloud.com>
2024-07-18 10:55:04 +0000

    updated build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+#python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate
+#python manage.py migrate

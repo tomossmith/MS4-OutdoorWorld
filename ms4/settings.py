8b92a14ee6538c8ebddc49f800489e44f1e649af
Tom Smith <tomossmith86@icloud.com>
2024-07-19 13:07:26 +0000

    changes in settings.py

--- ms4/settings.py
+++ ms4/settings.py
@@ -169,10 +169,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = (os.path.join(BASE_DIR, "static"),)
+STATIC = (os.path.join(BASE_DIR, "static"),)
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA = os.path.join(BASE_DIR, "media")
 
 # Send static and media files to AWS Bucket
 
